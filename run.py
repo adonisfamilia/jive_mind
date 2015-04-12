5e8d98bc7b404d2b231fd5d0920bde1a91eed3d1
Adonis Familia <adonis.familia.2@gmail.com>
2015-04-12 04:45:35 -0400

    first commit

---
+++ run.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, redirect
+import twilio.twiml
+import spotipy
+import requests
+import json
+
+
+app = Flask(__name__)
+
+sp = spotipy.Spotify()
+
+
+# Try adding your own number to this list!
+callers = {
+
+}
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond and greet the caller by name."""
+
+    from_number = request.values.get('From', None)
+    text_body = request.values.get('Body', None)
+    print(text_body)
+
+    if from_number in callers:
+        message = callers[from_number] + ", thanks for the message!"
+    else:
+        r = requests.get("https://api.spotify.com/v1/search?q=Black+Skinhead&type=track")
+        data = r.json()
+        message = data['tracks']['items'][0]["id"]
+        track = requests.get("https://api.spotify.com/v1/tracks/" + message)
+        track_json = track.json()
+        message = track_json['name']
+
+
+    resp = twilio.twiml.Response()
+    resp.message(message)
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
